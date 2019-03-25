3200beaf642cb9a207a4cdb34cb5e011da29d5d3
Bruce Nelson <bnelson.rugger@gmail.com>
2019-03-25 08:36:41 -0700

    Update: VariantSpark_HipsterIndex_Spark2.html notebooks

--- notebooks/Users/brnelson@microsoft.com/sample/adls gen2.scala
+++ notebooks/Users/brnelson@microsoft.com/sample/adls gen2.scala
@@ -6,11 +6,25 @@ spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "false")
 
 // COMMAND ----------
 
+spark.conf.set("fs.azure.account.auth.type", "OAuth")
+spark.conf.set("fs.azure.account.oauth.provider.type", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+spark.conf.set("fs.azure.account.oauth2.client.id", "8d15f4dd-3bc1-4bbd-ab06-c87876629c24")
+spark.conf.set("fs.azure.account.oauth2.client.secret", "TRQUrqVd/axlMGy4lmU/Po0uXqixt3gHSR+r8+KGwDk=")
+spark.conf.set("fs.azure.account.oauth2.client.endpoint", "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/token")
+spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "false")
+
+// COMMAND ----------
+
+display(dbutils.fs.ls("abfss://nifi@adbstorgen2.dfs.core.windows.net/test"))
+
+
+// COMMAND ----------
+
 // MAGIC %sh wget -P /tmp https://raw.githubusercontent.com/Azure/usql/master/Examples/Samples/Data/json/radiowebsite/small_radio_json.json
 
 // COMMAND ----------
 
-dbutils.fs.cp("file:///tmp/small_radio_json.json", "abfss://adlsgen2@adbstorgen2.dfs.core.windows.net/")
+dbutils.fs.cp("file:///tmp/small_radio_json.json", "abfss://nifi@adbstorgen2.dfs.core.windows.net/test")
 
 // COMMAND ----------
 
